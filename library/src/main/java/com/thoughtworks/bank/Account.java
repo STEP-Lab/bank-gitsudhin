e6ca52879e36a9d6d1fd6f2e1def97f8aac06a9d
Sudhin <sudhinmachad@gmail.com>
2018-03-19 19:15:32 +0530

    Added functionality to check balance ,minimum balance and account number.

---
+++ library/src/main/java/com/thoughtworks/bank/Account.java
@@ -0,0 +1,23 @@
+package com.thoughtworks.bank;
+
+public class Account {
+  private final String accountNumber;
+  private int balance;
+
+  public Account(String accountNumber, int balance) throws MinimumBalanceException {
+    this.accountNumber = accountNumber;
+    if (balance < 1000){
+      throw new MinimumBalanceException();
+    }
+    this.balance = balance;
+  }
+
+  public int getBalance() {
+    return balance;
+  }
+
+  public String getAccountNumber() {
+    return accountNumber;
+  }
+}
+  
---
+++ library/src/test/java/AccountTest.java
@@ -0,0 +1,32 @@
+import com.thoughtworks.bank.Account;
+import com.thoughtworks.bank.MinimumBalanceException;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class AccountTest {
+
+  private Account account;
+
+  @Before
+  public void setUp() throws MinimumBalanceException {
+    account = new Account("1234", 1000);
+  }
+
+  @Test
+  public void checkBalance(){
+    assertThat(account.getBalance(),is(1000));
+  }
+
+  @Test
+  public void checkAccountNumber() {
+    assertThat(account.getAccountNumber(),is("1234"));
+  }
+
+  @Test(expected = MinimumBalanceException.class)
+  public void checkMinimumBalance() throws MinimumBalanceException {
+    new Account("1235", 200);
+  }
+}
